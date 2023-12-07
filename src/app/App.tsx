ca30b13a8be689d08a41b3c7b0114c9d79fc26b9
Polina Lysikova <polinaly13@mail.ru>
2023-12-07 11:00:36 +0700

    Разработка страницы регистрации

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,13 +1,23 @@
 import './App.css';
-// import { HooksPage } from '../pages/HooksPage';
+import {Routes, Route, Link} from 'react-router-dom';
+import { HooksPage } from '../pages/HooksPage';
+import { HomePage } from '../pages/HomePage';
 import {LoginPage} from '../login/LoginPage';
 
 
 function App() {
   return (
-    <div className="App">
-      {/* <HooksPage /> */}
-      <LoginPage />
+    <div className="App"> 
+      <div style={{display: 'flex', width: '60%', justifyContent: 'space-between', margin: '50px auto 0'}}>
+        <Link to=" /" style={{display: 'block'}}>Home</Link>
+        <Link to=" /hooks" style={{display: 'block'}}>Hooks</Link>
+        <Link to=" /login" style={{display: 'block'}}>Login</Link>
+      </div>  
+    <Routes>
+      <Route path=" /" element={<HomePage />}></Route>
+      <Route path=" /hooks" element={<HooksPage />}></Route>
+      <Route path=" /login" element={<LoginPage />}></Route>
+    </Routes>
     </div>
   );
 }
--- src/login/LoginPage.jsx
+++ src/login/LoginPage.jsx
@@ -1,81 +1,105 @@
-import styled from "styled-components";
+import React, { useState } from "react";
 import {useForm} from "react-hook-form";
-import { useState } from "react";
 import axios from "axios";
-
-const StyledFrom = styled.section `
-    width: fit-content;
-    margin: 0 auto;
-
-    input {
-        display: block;
-    }
-
-    input:arror {
-        border-color: red;
-    }
-`;
+import "./LoginPage.css";
 
 export function LoginPage () {
-    const { register, formState: { errors } } = useForm();
-    const {data, setData} = useState({ email: "", password: "" });
+  const { register, formState: { errors } } = useForm();
+  const [data, setData] = useState({ email: "", password: "" });
+  const [formValid, setFormValid] = useState(false);
+  const [emailValid, setEmailValid] = useState("");
+  const [formValidText, setFormValidText] = useState("");
+  const [emailResult, setEmailResult] = useState("");
+  const [passwordResult, setPasswordResult] = useState("");
+  const [disabledBtn, setDisabledBtn] = useState(false);
 
-    const [emailValid, setEmailValid] = useState("");
-    const [formValid, setFormValid] = useState(false);
-    const [response, setResponse] = useState("");
+  const validateEmail = (event) => {
+    var regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
 
-    const validateEmail = (event) => {
-        var regex = /^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$/;
-
-            if ((!regex.test(event.target.value)) && (event.target.value.length !== 0)) {
-                setFormValid(false);
-                setEmailValid("Почта написана некорректно");
-            } else {
-                setEmailValid("");
-                setFormValid(true);
-            }
-    };
-
-    const handleChange = (event) => {
-        setData({ ...data, [event.target.name]: event.target.value });
-    };
-
-    const handleSubmit = (event) => {
-        event.preventDefault();
-        if (formValid) {
-            axios
-            .post("https://jsonplaceholder.typicode.com/users", data)
-            .then((response) => {
-                setResponse(response.data);
-            })
-            .catch((error) => {
-                console.log(error);
-            });
-        };
-    }
-
-    return (
-        <StyledFrom>
-        <h2>Registration form</h2>
-        <form onSubmit={handleSubmit}>
-            <input type="email" name="email" value={data.email} {...register("mail", { required: true })} onBlur={validateEmail}  onChange={handleChange} placeholder="example@mail.ru"/>
-            <p style={{margin: 0, color: "red"}}>{emailValid}</p>
-                {errors.mail && <span style={{ color: "red" }}>
-                    Почта обязательна для заполнения </span>}
-           
-            <input type="password" name="password" value={data.password} {...register("pass", { required: true })} onChange={handleChange} placeholder="********"/>
-                {errors.pass && <span style={{ color: "red" }}>
-                    Пароль обязателен для заполнения </span>}
-            <input type={"submit"} />
-        </form>
-        {response && (
-            <div>
-                <p>Данные успешно отправлены:</p>
-                <p>Почта:{response.email}</p>
-                <p>Пароль:{response.password}</p>
-            </div>
-            )
+        if ((!regex.test(event.target.value)) && (event.target.value.length !== 0)) {
+            setFormValid(false);
+            setEmailValid("Почта написана некорректно");
+        } else {
+            setEmailValid("");
+            setFormValid(true);
         }
-        </StyledFrom>
-    )
-}
+  };
+
+  const handleChange = (e) => {
+    const value = e.target.value;
+    setData({ ...data, [e.target.name]: value });
+  };
+
+  const renderResults = (response) => {
+    setEmailResult(["Почта: ",response.data.email]);
+    setPasswordResult(["Пароль: ", response.data.password]);
+    setData({ email: "", password: "" });
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const userData = { email: data.email, password: data.password };
+    if (!formValid) {
+      return setFormValidText("Форма не валидна");
+    } else{
+      setDisabledBtn(true);
+      async function asyncFunction() {
+        try {
+            const response = await
+            axios
+            .post(process.env.REACT_APP_BFF_URL, userData);
+            setDisabledBtn(false);
+            if (!response) {
+              throw Error(response.statusText);
+            } else {
+              console.log(response.data.email, response.data.password);
+              renderResults(response);
+            }
+        } catch (error) {
+            console.log(error);
+        }
+      }
+        asyncFunction();
+    }
+  };
+
+  return (
+    <div className="LoginPage">
+      <h1>Регистрация</h1>
+      <form onSubmit={handleSubmit} className="form">
+        <label htmlFor="email">
+          Почта
+          <input
+          type="email"
+          name="email"
+          {...register("email", { 
+            required: {
+              value: true,
+              message: 'Поле обязательно для заполнения'
+          }})}
+            value={data.email}
+            onChange={handleChange}
+            onBlur={validateEmail}           
+          />
+        </label>
+        <p style={{margin: 0, color: "red"}}>{emailValid}</p>
+        {errors.email && 
+        <span style={{ color: "red" }}>{errors.email.message}</span>}
+        <label htmlFor="password">
+          Пароль
+          <input
+            type="password"
+            name="password"
+            value={data.password}
+            onChange={handleChange}
+            required
+          />
+        </label>
+        <button type="submit" disabled={disabledBtn}>Отправить</button>
+      </form>
+      <p>{formValidText}</p>
+      <p>{emailResult}</p>
+      <p>{passwordResult}</p>
+    </div>
+  );
+};
